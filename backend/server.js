e7a4fa38e798866cfe746a6351cccdd0fc014ccb
aarnav1729 <aarnavsingh836@gmail.com>
2024-09-04 13:50:27 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -48,15 +48,14 @@ app.post('/api/racks', async (req, res) => {
 
     const existingRack = await Rack.findOne({ packages: qrCode });
     if (existingRack) {
-      // Return a 200 OK response instead of a 400 error
-      return res.status(200).json({ 
+      return res.status(400).json({ 
         message: 'QR Code already exists.', 
         location: {
           column: existingRack.column,
           row: existingRack.row,
           stack: existingRack.stack
         }
-      });
+      }); 
     }
 
     let rack = await Rack.findOne({ column, row, stack });
