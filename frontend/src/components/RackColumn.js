80852c53766102ad1626bdd940c41df322b198c3
aarnav1729 <aarnavsingh836@gmail.com>
2024-09-04 13:22:45 +0530

    contain col

--- frontend/src/components/RackColumn.js
+++ frontend/src/components/RackColumn.js
@@ -5,7 +5,7 @@ const RackColumn = ({ column, racks, fetchRacks, highlightedSpot, handleSearch }
   const rows = Array.from({ length: 33 }, (_, index) => index + 1);
 
   return (
-    <div className="rack-column border flex flex-col items-center bg-gray-900">
+    <div className="rack-column border flex flex-col items-center bg-gray-900 object-contain">
       <h2 className="text-center font-bold mb-4 text-white">{column}</h2> 
       <div className="grid grid-cols-1 w-full">
         {rows.map(row => (
