82a5c2472c410bc1ad3f54422c38d3a4ca966fd6
aarnav1729 <aarnavsingh836@gmail.com>
2024-09-05 11:44:18 +0530

    published

--- frontend/src/components/Empty.js
+++ frontend/src/components/Empty.js
@@ -11,18 +11,26 @@ const Empty = ({ racks = [], emptySpots = [] }) => {
         <div key={column} className="rack-column border flex flex-col items-center bg-gray-900 object-contain rounded-lg shadow-lg p-4">
           <h2 className="text-center font-bold mb-4 mt-2 text-gray-900 bg-white rounded-full p-4">{column}</h2>
           <div className="grid grid-cols-1 w-full">
-            {racks.filter(rack => rack.column === column).map(rack => (
-              rack.packages.length === 0 && emptySpots.some(spot =>
-                spot.column === rack.column && spot.row === rack.row && spot.stack === rack.stack
-              ) && (
-                <div
-                  key={`${rack.column}-${rack.row}-${rack.stack}`}
-                  className="rack-spot border flex flex-col justify-between items-center bg-gray-700 rounded-lg shadow-md p-4 mb-4"
-                >
-                  <h3 className="text-center text-white font-bold mb-2">{`${rack.column} - Row: ${rack.row}, Stack: ${rack.stack}`}</h3>
-                  <p className="text-white text-center">No QR Codes</p>
-                </div>
-              )
+            {Array.from({ length: 33 }, (_, rowIndex) => rowIndex + 1).map(row => ( // Loop over each row
+              <div key={`${column}-${row}`} className="w-full">
+                {Array.from({ length: 5 }, (_, stackIndex) => stackIndex + 1).map(stack => { // Loop over each stack
+                  const isEmpty = emptySpots.some(spot =>
+                    spot.column === column && spot.row === row && spot.stack === stack
+                  );
+
+                  return (
+                    isEmpty && (
+                      <div
+                        key={`${column}-${row}-${stack}`}
+                        className="rack-spot border flex flex-col justify-between items-center bg-gray-700 rounded-lg shadow-md p-4 mb-4"
+                      >
+                        <h3 className="text-center text-white font-bold mb-2">{`${column} - Row: ${row}, Stack: ${stack}`}</h3>
+                        <p className="text-white text-center">No QR Codes</p>
+                      </div>
+                    )
+                  );
+                })}
+              </div>
             ))}
           </div>
         </div>
