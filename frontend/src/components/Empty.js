8594c77b486148ca7809200e722dbb4f36537e14
aarnav1729 <aarnavsingh836@gmail.com>
2024-09-05 13:26:58 +0530

    updated

--- frontend/src/components/Empty.js
+++ frontend/src/components/Empty.js
@@ -4,14 +4,14 @@ import RackColumn from './RackColumn';
 const Empty = ({ racks, fetchRacks }) => {
   const columns = ['B1', 'B2', 'B3', 'B4'];
 
-  // Calculate full spots directly within this component
+  // calculate full spots directly within this component
   const fullSpots = racks.filter(rack => rack.packages.length === 2).map(rack => ({
     column: rack.column,
     row: rack.row,
     stack: rack.stack,
   }));
 
-  console.log(fullSpots);
+  console.log('these are spots with exactly 2 qr codes: ',fullSpots);
 
   return (
     <div className="empty-system grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-2">
@@ -21,7 +21,7 @@ const Empty = ({ racks, fetchRacks }) => {
             column={column}
             racks={racks.filter(rack => rack.column === column)}
             fetchRacks={fetchRacks}
-            fullSpots={fullSpots}  // Pass fullSpots directly to RackColumn
+            fullSpots={fullSpots}  // pass fullSpots directly to RackColumn
           />
         </div>
       ))}
